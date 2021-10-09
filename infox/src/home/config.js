874188e1f810bc428a12c88ce268d8a3b7aafdab
Mariaclara2005 <mariaclara34campos@gmail.com>
2021-10-09 13:28:09 -0300

    ok

--- infox/src/home/config.js
+++ infox/src/home/config.js
@@ -1,5 +1,21 @@
+
 const CarouselConfig = {
-    superLargueDesktop: {
-        breackpoint
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
     }
-}
+  };
+  
+  export { CarouselConfig }
